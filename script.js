78744b5afd2f9f1baed2772a14f4739b88771075
Shun <85538338+iwashun22@users.noreply.github.com>
2023-08-13 20:14:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,185 @@
 $(() => {
   let rotateInterval;
-  const intervalTimeOut = 70;
-  let degreeX = 0, degreeY = 0;
-
-  rotateInterval = setInterval(rotateAnimation, intervalTimeOut);
+  const intervalTimeOut = 10;
+  const degree = {
+    currentX: 0,
+    currentY: 0,
+    currentZ: 0,
+    toX: 0,
+    toY: 0,
+    toZ: 0,
+    directionX: 0,
+    directionY: 0,
+    directionZ: 0
+  };
+  let clickedFrontSide; // bool
 
   const cube = $(".cube");
-  
-  function rotateAnimation() {
-    degreeX += 1;
-    degreeY += 1;
-    degreeX %= 360;
-    degreeY %= 360;
-    cube.css("transform", `rotateX(${degreeX}deg) rotateY(${degreeY}deg)`);
-  }
+  const everySides = $(".cube div");
 
   function addClickEvent() {
-    $(".cube div").each(function(index, element) {
+    everySides.addClass("hover-effect");
+    everySides.removeClass("no-effect");
+
+    everySides.each(function(index, element) {
       $(element).on("click", function(e) {
-        clearInterval(rotateInterval);
-        $(".cube div").off("click");
-        if($(this).hasClass("front")) {
-          rotateTo(0, 0);
-          return;
-        }
-        if($(this).hasClass("back")) {
-          rotateTo(0, 180);
-          return;
-        }
-        if($(this).hasClass("right")) {
-          rotateTo(0, 270);
-          return;
-        }
-        if($(this).hasClass("left")) {
-          rotateTo(0, 90);
-          return;
-        }
-        if($(this).hasClass("top")) {
-          rotateTo(270, 0);
-          return;
-        }
-        if($(this).hasClass("bottom")) {
-          rotateTo(90, 0)
-          return;
+        everySides.removeClass("hover-effect");
+        everySides.addClass("no-effect")
+        $(this).addClass("clicked");
+        everySides.off("click"); // remove click event while rotating to the clicked side
+        [ degree.currentX, degree.currentY, degree.currentZ ] = getRotationAngle(document.getElementById("cube"));
+        console.log(degree.currentX, degree.currentY, degree.currentZ)
+        // console.log(rotationX, rotationY);
+        const side = $(this).data("side");
+        clickedFrontSide = side === "front";
+        switch(side) {
+          case "front":
+            rotateTo(0, 0)
+            break;
+          case "back":
+            rotateTo(0, 180)
+            break;
+          case "right":
+            rotateTo(0, 270)
+            break;
+          case "left":
+            rotateTo(0, 90)
+            break;
+          case "top":
+            rotateTo(270, 0)
+            break;
+          case "bottom":
+            rotateTo(90, 0)
+            break;
         }
       });
     });
   }
   addClickEvent();
 
-  function rotateTo(x, y, directionX, directionY, stopX, stopY) {
-    if(!directionX) {
-      let gap = Math.abs(degreeX - x);
-      directionX = (x < degreeX && gap < 180) || (gap > 180 && x > degreeX) ? -1 : 1;
-      // directionX = ((x > degreeX && gap < 180) || (x < degreeX && gap > 180)) ? 1 : -1;
+  function rotateTo(toX, toY, toZ = 0) {
+    degree.directionX = findClosestRotateDirection(degree.currentX, toX);
+    degree.directionY = findClosestRotateDirection(degree.currentY, toY);
+    degree.directionZ = findClosestRotateDirection(degree.currentZ, toZ);
+    // console.log(degree.directionX, degree.directionY);
+    degree.toX = toX;
+    degree.toY = toY;
+    degree.toZ = toZ;
+
+    console.log(degree);
+    cube.css("animation", "none")
+
+    cube.css("transform", `rotateX(${degree.currentX}deg) rotateY(${degree.currentY}deg) rotateZ(${degree.currentZ}deg)`);
+    rotateInterval = setInterval(rotateAnimation, intervalTimeOut);
+  }
+
+  function rotateAnimation() {
+    if(degree.currentX === degree.toX && degree.currentY === degree.toY && degree.currentZ === degree.toZ) {
+      clearInterval(rotateInterval);
+        $(".clicked").removeClass("clicked");
+        if(!(degree.currentX === 0 && degree.currentY === 0)) {
+          console.log("move back to front")
+          setTimeout(() => {
+            rotateTo(0, 0)
+          }, 3000)
+          return;
+        } 
+        
+        if(!clickedFrontSide) {
+          addClickEvent();
+          cube.css("animation", "rotate 20s infinite linear");
+        } else {
+          setTimeout(() => {
+            addClickEvent();
+            cube.css("animation", "rotate 20s infinite linear");
+          }, 3000)
+        }
+      
+      return;
     }
-    if(!directionY) {
-      let gap = Math.abs(degreeY - y);
-      directionY = (y < degreeY && gap < 180) || (gap > 180 && y > degreeY) ? -1 : 1;
-      // directionY = ((y > degreeY && gap < 180) || (y < degreeY && gap > 180)) ? 1 : -1;
+
+    if(degree.currentX !== degree.toX) {
+      degree.currentX += degree.directionX;
+      degree.currentX %= 360;
+
+      if(degree.currentX < 0) degree.currentX += 360;
     }
-    // console.log(directionX, directionY);
-    if(!stopX) {
-      degreeX += directionX;
-      degreeX %= 360;
+
+    if(degree.currentY !== degree.toY) {
+      degree.currentY += degree.directionY;
+      degree.currentY %= 360;
+
+      if(degree.currentY < 0) degree.currentY += 360;
     }
-    if(!stopY) {
-      degreeY += directionY;
-      degreeY %= 360;
+
+    if(degree.currentZ !== degree.toZ) {
+      degree.currentZ += degree.directionZ;
+      degree.currentZ %= 360;
+
+      if(degree.currentZ < 0) degree.currentZ += 360;
     }
-    cube.css("transform", `rotateX(${degreeX}deg) rotateY(${degreeY}deg)`);
-    let sameX = degreeX < 0 ? (degreeX + 360) == x : degreeX == x;
-    let sameY = degreeY < 0 ? (degreeY + 360) == y : degreeY == y;
-    if(!sameX || !sameY) {
-      // console.log(degreeX, x);
-      // console.log(degreeY, y, sameY, directionY);
-      setTimeout(() => { rotateTo(x, y, directionX, directionY, sameX, sameY) }, 10)
-    }
-    else {
-      setTimeout(() => { 
-        rotateInterval = setInterval(rotateAnimation, intervalTimeOut) ;
-        addClickEvent();
-      }, 3000);
-    }
+
+    // console.log(degree.currentX, degree.currentY);
+    cube.css("transform", `rotateX(${degree.currentX}deg) rotateY(${degree.currentY}deg) rotateZ(${degree.currentZ}deg)`);
   }
 })
+
+/**
+ * 
+ * @param {Number<Int16Array>} currentDegree 
+ * @param {Number<Int16Array>} endPoint 
+ */
+function findClosestRotateDirection(currentDegree, endPoint) {
+  let gap;
+  if(currentDegree < endPoint) {
+    gap = endPoint - currentDegree;
+
+    return gap <= 180 ?
+      1 : // increase
+      -1; // decrease
+  } else {
+    gap = currentDegree - endPoint;
+
+    return gap <= 180 ?
+      -1 : // decrease
+      1; // increase
+  }
+}
+
+function getRotationAngle(target) 
+{
+  const obj = window.getComputedStyle(target, null);
+  const matrix = obj.getPropertyValue('-webkit-transform') || 
+    obj.getPropertyValue('-moz-transform') ||
+    obj.getPropertyValue('-ms-transform') ||
+    obj.getPropertyValue('-o-transform') ||
+    obj.getPropertyValue('transform');
+
+  let rotationX = 0, rotationY = 0, rotationZ = 0;
+
+  if (matrix !== 'none') 
+  {
+    const matrixArr = matrix.split('(')[1].split(')')[0].split(',').map(e => Number(e));
+    rotationX = Math.atan2(matrixArr[9], matrixArr[10]);
+    rotationY = Math.atan2(-matrixArr[8], 
+                            Math.sqrt(matrixArr[0] ** 2 + matrixArr[4] ** 2));
+    rotationZ = Math.atan2(matrixArr[1], matrixArr[0]);
+  } 
+
+  // Convert angles to degrees and adjust to full 360-degree range
+  function toDegrees(angle) {
+    return (angle < 0 ? 360 + (angle * 180 / Math.PI) : angle * 180 / Math.PI);
+  }
+
+  let degreeX = Math.round(toDegrees(rotationX)) % 360;
+  let degreeY = Math.round(toDegrees(rotationY)) % 360;
+  let degreeZ = Math.round(toDegrees(rotationZ)) % 360;
+
+  return [360 - degreeX, 360 - degreeY, 360 - degreeZ];
+}
+
+// setInterval(() => {
+//   const cube = document.getElementById("cube");
+//   [x, y, z] = getRotationAngle(cube);
+//   console.log(x, y, z);
+// }, 100)
